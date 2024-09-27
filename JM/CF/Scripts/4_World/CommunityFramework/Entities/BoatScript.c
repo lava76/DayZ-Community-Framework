4b335b38067ce9a066bce8e9385d27f7c8f151c3
lava76 <lava76+github@mail.cbw-clan.de>
2024-09-27 11:36:33 +0200

    Added BoatScript CF_ModStorage support

---
+++ JM/CF/Scripts/4_World/CommunityFramework/Entities/BoatScript.c
@@ -0,0 +1,40 @@
+#ifndef DAYZ_1_25
+modded class BoatScript
+{
+#ifdef CF_MODSTORAGE
+	autoptr CF_ModStorageBase m_CF_ModStorage = new CF_ModStorageObject<BoatScript>(this);
+
+	override void OnStoreSave(ParamsWriteContext ctx)
+	{
+		super.OnStoreSave(ctx);
+
+		m_CF_ModStorage.OnStoreSave(ctx);
+	}
+
+	override bool OnStoreLoad(ParamsReadContext ctx, int version)
+	{
+		if (!super.OnStoreLoad(ctx, version))
+		{
+			return false;
+		}
+
+		return m_CF_ModStorage.OnStoreLoad(ctx, version);
+	}
+#endif
+
+	/**
+	 * @brief Refer to CF/ModStorage implementation of ItemBase::CF_OnStoreSave
+	 */
+	void CF_OnStoreSave(CF_ModStorageMap storage)
+	{
+	}
+
+	/**
+	 * @brief Refer to CF/ModStorage implementation of ItemBase::CF_OnStoreLoad
+	 */
+	bool CF_OnStoreLoad(CF_ModStorageMap storage)
+	{
+		return true;
+	}
+};
+#endif
